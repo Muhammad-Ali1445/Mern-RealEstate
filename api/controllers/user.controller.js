a4a8dcf61fc96d39eef818909dbb6e83e446e712
Ali Munir <alimunir1445@gmail.com>
2024-08-28 16:08:49 +0500

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+  res.send("respnse form test");
+};
--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"; // Ensure the .js extension is correct
+
 dotenv.config();
 const app = express();
+
 mongoose
   .connect(process.env.MONGO)
-  
   .then(() => {
     console.log("Connected to MongoDB!");
   })
-  .catch((err) => console.log("Error connecting): " + err.message));
+  .catch((err) => console.log("Error connecting: " + err.message));
+
+app.use("/api/user", userRouter);
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!");
 });
