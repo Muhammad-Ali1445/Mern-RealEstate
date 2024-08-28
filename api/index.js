7042d5845215933b04e8dc4f9288b5d8d3501c24
Ali Munir <alimunir1445@gmail.com>
2024-08-28 12:22:18 +0500

    create db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => console.log("Error connecting): " + err.message));
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!");
 });
