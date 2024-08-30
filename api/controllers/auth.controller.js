4554012b6df79bf70cbf4c6c01d031974e2f561d
Ali Munir <alimunir1445@gmail.com>
2024-08-30 14:57:37 +0500

    create signIn api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   let { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    return res.status(400).json({ success: false, message: "All fields are required" });
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -13,8 +16,55 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.status(201).json({ success: true, message: "User registered successfully" });
+    res
+      .status(201)
+      .json({ success: true, message: "User registered successfully" });
   } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  let { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const validUser = await User.findOne({ email: email });
+
+    if (!validUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Wrong Credentials" });
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h", // Token expiration time
+    });
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      })
+      .status(200)
+       .json({ success: true, message: "Login successful", user: rest });
+      // .json(rest);
+  } catch (error) {
+    console.error(error);
     next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { signup } from "../controllers/auth.controller.js";
+import { signIn } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/sign-in",signIn);
 
 export default router;
