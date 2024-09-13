9b8768a13837df3638f0e4d37b9e2069795eee3d
Ali Munir <alimunir1445@gmail.com>
2024-09-13 16:16:23 +0500

    complete showUser listing Functionality

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -52,7 +52,7 @@ const listingSchema = new mongoose.Schema(
       type: Boolean,
       required: true,
     },
-    imageUrl: {
+    imageUrls: {
       type: Array,
       required: true,
     },
