5200dcb03d0cf4e2a6d2bee3f7dd572c236313cd
Ali Munir <alimunir1445@gmail.com>
2024-09-19 18:57:19 +0500

    add img slider to listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
+const Listing = () => {
+  SwiperCore.use([Navigation]);
+  const params = useParams();
+  const [loading, setLoading] = useState(false);
+  const [listing, setListing] = useState(null);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    try {
+      setLoading(true);
+      const fetchListing = async () => {
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+      };
+      fetchListing();
+    } catch (error) {
+      setError(true);
+      loading(false);
+    }
+  }, [params.listingId]);
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something Went Wrong</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default Listing;
