186ae9418e81bb55a740f3b0c7f805929872ed70
Ali Munir <alimunir1445@gmail.com>
2025-07-16 01:28:24 +0500

    Removed client static file serving to fix 404 error on Render

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import path from 'path';
+import path from "path";
+
 dotenv.config();
 
 mongoose
@@ -16,29 +17,25 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
- 
-  const __dirname = path.resolve();
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
-});
-
+// API routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+// ❌ Commented out because we're not serving the frontend from here
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -47,4 +44,8 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
 });
