e873a071c476ec84fe492517e9d0cb0fb06f0e95
Ali Munir <alimunir1445@gmail.com>
2025-08-27 15:44:20 +0500

    fix: add credentials include for landlord fetch and fix React key warning
    - Added `credentials: "include"` to user fetch in Contact.jsx to send auth cookies
    - Moved key prop to <SwiperSlide> in Home.jsx to resolve unique key warning

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,7 +47,7 @@ export default function Home() {
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        log(error);
+        console.log(error);
       }
     };
     fetchOfferListings();
@@ -83,14 +83,13 @@ export default function Home() {
         {offerListings &&
           offerListings.length > 0 &&
           offerListings.map((listing) => (
-            <SwiperSlide>
+            <SwiperSlide key={listing._id}>
               <div
                 style={{
-                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  background: `url(${listing.imageUrls[1]}) center no-repeat`,
                   backgroundSize: "cover",
                 }}
                 className="h-[500px]"
-                key={listing._id}
               ></div>
             </SwiperSlide>
           ))}
